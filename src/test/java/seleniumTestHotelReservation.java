57563250c9359175e64688d5655645aea9012dcf
ABsid0017 <SP19BSSE0017@MAJU.EDU.PK>
2022-12-29 15:43:00 +0500

    check method

--- src/test/java/seleniumTestHotelReservation.java
+++ src/test/java/seleniumTestHotelReservation.java
@@ -12,20 +12,14 @@ public class seleniumTestHotelReservation {
 
     @Test
     public void LoginTest() {
-
         driver = new ChromeDriver();
 
-        //maximize the browser
         driver.manage().window().maximize();
 
-        //Implicit wait, wait for at least some time (10 sec) to identify an element, //if can't find the element with in 10
-
-        //open the url or AUT
         driver.get("http://adactinhotelapp.com/HotelAppBuild2/index.php");
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        //Click on the register link:
         driver.findElement(By.id("username")).sendKeys("SP19BSSE0017");
 
         driver.findElement(By.id("password")).sendKeys("111");
@@ -33,10 +27,17 @@ public class seleniumTestHotelReservation {
         driver.findElement(By.id("login")).click();
 
         String welcomeTag = driver.findElement(By.cssSelector("body:nth-child(2) table.content:nth-child(2) tbody:nth-child(1) tr:nth-child(1) > td.welcome_menu:nth-child(1)")).getText();
+
         Assert.assertEquals(welcomeTag,"Welcome to Adactin Group of Hotels");
 
         driver.quit();
         driver=null;
+    }
+
+    @Test
+    public void checkTest() {
 
     }
+
+
 }
